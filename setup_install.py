554ffb79311a8003afeb8543cb3c599a9c14ccc6
Fabian Barraez <fabianbarraez@gmail.com>
2021-05-30 22:33:48 -0500

    feat: add installer script

---
+++ setup_install.py
@@ -0,0 +1,30 @@
+import os, time 
+
+
+def install_node():
+    os.system('curl -sL https://deb.nodesource.com/setup_15.x | bash -')
+    os.system('apt-get install -y nodejs')
+    os.system('clear')
+
+def install_kivy():
+    os.system('sudo apt update')
+    os.system('sudo apt install pkg-config libgl1-mesa-dev libgles2-mesa-dev \
+            libgstreamer1.0-dev \
+            gstreamer1.0-plugins-{bad,base,good,ugly} \
+            gstreamer1.0-{omx,alsa} libmtdev-dev \
+            xclip xsel libjpeg-dev')
+    os.system('sudo apt install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev')
+    os.system('python -m pip install kivy[base] kivy_examples')
+
+def setup_touchscreen_input():
+    os.system("sed -i '/\[input\]/a hid_%(name)s = probesysfs,provider=hidinput' ~/.kivy/config.ini")
+    os.system("sed -i '/\[input\]/a mtdev_%(name)s = probesysfs,provider=mtdev' ~/.kivy/config.ini")
+
+if __name__ == '__main__':
+    try:
+        install_node() # install node 
+        install_kivy() # install kivy]
+        setup_touchscreen_input() # enable touchscreen 
+
+    except Exception as e:
+        print(f"it does not work: {e}")
