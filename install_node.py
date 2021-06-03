a7a89f330a62ba22f5cabab34dbe53f4fe510e7c
fabocode <fabianbarraez@gmail.com>
2021-06-03 11:39:41 -0500

    add install node comments

--- install_node.py
+++ install_node.py
@@ -1,8 +1,10 @@
 import os 
 
 def install_node():
+    '''
+        This script install node js to the raspberry pi
+    '''
     os.system('sudo apt update')
     os.system('curl -sL https://deb.nodesource.com/setup_15.x | bash -')
     os.system('apt-get install -y nodejs')
     os.system('clear')
-    
