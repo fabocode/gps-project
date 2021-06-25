d3fa782bd0e451be54c159d01c19e9bd52fd0824
Fabian <fabianbarraez@gmail.com>
2021-06-25 15:22:08 -0500

    feat: 🎸 add main function

---
+++ src/gps_system/__init__.py
@@ -0,0 +1,6 @@
+"""GPS Tracking System
+By Fabian Barraez fabianbarraez@gmail.com
+
+GPS Tracking System for Racing Applications """
+
+__version__ = '0.1.0'
---
+++ src/gps_system/__main__.py
@@ -0,0 +1,8 @@
+import gps_system
+from gps_system import gps_system
+
+def main():
+    gps_system.run()
+
+if __name__ == '__main__':
+    main()
