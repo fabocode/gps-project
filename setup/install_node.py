5774a03708620bcf485d4d18834f9415ef415310
Fabian <fabianbarraez@gmail.com>
2021-06-28 17:40:52 -0500

    feat: 🎸 install node script

---
+++ setup/install_node.py
@@ -0,0 +1,13 @@
+'''
+    NOTE: RUN WITH `sudo`
+'''
+import os 
+
+def install_node():
+    '''
+        This script install node js to the raspberry pi
+    '''
+    os.system('sudo apt update')
+    os.system('curl -sL https://deb.nodesource.com/setup_15.x | bash -')
+    os.system('apt-get install -y nodejs')
+    os.system('clear')
