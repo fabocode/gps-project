5ce31bdc689481caee56b38b7be079d5d4ee47fb
Fabian <fabianbarraez@gmail.com>
2021-07-05 15:37:09 -0500

    style: 💄 remove unused comments

--- setup/installer.sh
+++ setup/installer.sh
@@ -2,11 +2,6 @@
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
 
-#cd /
-#bluetoothctl << EOF
-#power on
-#EOF
-
 cd /
 cd /home/pi/gps-project/setup/
 sudo pip3 install -r requirements.txt
